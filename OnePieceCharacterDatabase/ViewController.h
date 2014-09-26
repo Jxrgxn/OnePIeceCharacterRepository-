cfd31506ef909a2369c951901f5ca9bf7f8eeef5
KacheMoney <basel.farag@gmail.com>
2014-09-26 18:54:34 -0500

    First Commit

---
+++ OnePieceCharacterDatabase/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OnePieceCharacterDatabase
+//
+//  Created by Basel Farag on 9/26/14.
+//  Copyright (c) 2014 NextTierGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OnePieceCharacterDatabase/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OnePieceCharacterDatabase
+//
+//  Created by Basel Farag on 9/26/14.
+//  Copyright (c) 2014 NextTierGames. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
